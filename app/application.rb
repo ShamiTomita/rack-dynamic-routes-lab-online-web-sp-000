0e53d158b73ff58eea89316c0b3add9ef61a8b47
Shami Tomita <shamitomita@gmail.com>
2021-04-28 20:03:39 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,6 +5,7 @@ class Application
     
     if req.path.match(/items\/.+/)
         item_name = req.path.split("/items/").last
+        @@items
         resp.write "#{item.price}"
       end
     else 
