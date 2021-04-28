a33dbb8b9b53fc6e478c6468f5cd9cbed8d0df30
Shami Tomita <shamitomita@gmail.com>
2021-04-28 20:01:08 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,6 +4,7 @@ class Application
     req = Rack::Request.new(env)
     
     if req.path.match(/items/)
+      @@items.each do |item|
       resp.write "#{item.price}"
     else 
       resp.write "Route not found"
